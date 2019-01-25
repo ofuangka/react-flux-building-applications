372ff33fb7832190314b6674471a6773994f3439
Osha Fuangkasae <ofuangka@gmail.com>
2019-01-25 17:20:20 -0600

    Initial commit

---
+++ src/api/authorData.js
@@ -0,0 +1,19 @@
+module.exports = {
+    authors: [
+        {
+            id: 'cory-house',
+            firstName: 'Cory',
+            lastName: 'House'
+        },
+        {
+            id: 'scott-alan',
+            firstName: 'Scott',
+            lastName: 'Alan'
+        },
+        {
+            id: 'dan-wahlin',
+            firstName: 'Dan',
+            lastName: 'Wahlin'
+        }
+    ]
+};
